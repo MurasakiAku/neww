dc2d8f4d61df4d5a0b02d6fb17d30607d89801ed
MurasakiAku <Nastya-13-14@yandex.ru>
2023-11-12 23:23:36 +0700

    new
    new

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -9,42 +9,72 @@ namespace ConsoleApp3
 {
     class Program
     {
-        public class PrintWhile
-        {
-            public static int PrintResultWhile(int n)
-            {
-                int k = 1;
-                while (k * k < n)
+        //public class PrintWhile
+        //{
+        //    public static int PrintResultWhile(int n)
+        //    {
+        //        int k = 1;
+        //        while (k * k < n)
+        //        {
+        //            k++;
+        //        }
+        //        return k;
+        //    }
+        //}
+        //public class PrintFor
+        //{
+        //    public static double PrintResultFor(double d)
+        //    {
+        //        double w = 0;
+        //        for (int i = 1; i <= d; i++)
+        //        {
+        //            w = (Math.Pow(d, 2) + Math.Pow(d + i, 2) + Math.Pow(2 * d, 2));
+        //        }
+        //        return w;
+        //    }
+        //}
+
+        public class PrintResult {
+
+           public static bool WriteLineOne(int b)
+           {
+                int[] a;
+
+                a = new int[b];
+                Console.WriteLine("Введите числа:");
+                for (int i = 0; i < b; i++)
                 {
-                    k++;
+                    a[i] = int.Parse(Console.ReadLine());
                 }
-                return k;
-            }
-        }
-        public class PrintFor
-        {
-            public static double PrintResultFor(double d)
-            {
-                double w = 0;
-                for (int i = 1; i <= d; i++)
+                for (int i = 0; i < b - 1; i++)
                 {
-                    w = (Math.Pow(d, 2) + Math.Pow(d + i, 2) + Math.Pow(2 * d, 2));
+                    if (a[i] >= a[i + 1])
+                    {
+                        return false;
+                    }
                 }
-                return w;
-            }
+                return true;
+           }
         }
+    
+
 
         static void Main(string[] args)
         {
-            PrintWhile printWhile = new PrintWhile();
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine(PrintWhile.PrintResultWhile(n));
+            //PrintWhile printWhile = new PrintWhile();
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine(PrintWhile.PrintResultWhile(n));
 
-            PrintFor printFor = new PrintFor();
-            double d = double.Parse(Console.ReadLine());
-            Console.WriteLine(PrintFor.PrintResultFor(d));
+            //PrintFor printFor = new PrintFor();
+            //double d = double.Parse(Console.ReadLine());
+            //Console.WriteLine(PrintFor.PrintResultFor(d));
 
-
+            PrintResult printResult = new PrintResult();
+            Console.WriteLine("Количество вводимых чисел:");
+            int b = int.Parse(Console.ReadLine());
+            //int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintResult.WriteLineOne(b));
+            
             Console.ReadKey();
         }
         
